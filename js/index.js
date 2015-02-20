3d0950fabcce9ad30d95b9480c01024c4dad0614
aubrey <aubrey@leentechsystems.com>
2015-02-20 16:37:44 +0800

    modified:   config.xml 	modified:   index.html 	modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -53,7 +53,7 @@ function onOnline() {
 
      setTimeout(function(){
             //var ref = window.open('http://m.meiceljewelry.com/', '_blank', 'location=no');
-            var ref = window.location = "http://terracehillresort.com/index.php";
+            var ref = window.location = "http://m.townsdelight.com/index.php";
             var temp_url = '';
             ref.addEventListener('loaderror', function(event) { ref.close(); location.reload(); });
             ref.addEventListener('loadstart', function(event) { load_url(event.url); });
@@ -63,7 +63,7 @@ function onOnline() {
 function load_url(url) {
     var base_domain = url.substr(0, 26);
     alert(url);
-    if(base_domain != 'http://terracehillresort.com/index.php') {
+    if(base_domain != 'http://m.townsdelight.com/index.php') {
         var external = window.open(url, '_system', 'location=no');
         return true;
     }
